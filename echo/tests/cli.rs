501e8d7e4ccb3a2616d24aba5c35370b6414cd80
Yusuke YATSUO <yyatsuo@gmail.com>
2024-05-01 10:37:54 +0900

    Update some

--- echo/tests/cli.rs
+++ echo/tests/cli.rs
@@ -1,16 +1,25 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
 
 #[test]
 fn dies_no_args() {
-  let mut cmd = Command::cargo_bin("echo").unwrap();
-  cmd.assert()
-    .failure()
-    .stderr(predicate::str::contains("USAGE"));
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
 }
 
 #[test]
 fn runs() {
-    let mut cmd = Command::cargo_bin("echo").unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() {
+  let outfile = "tests/expected/hello1.txt";
+  let expected = fs::read_to_string(outfile).unwrap();
+  let mut cmd = Command::cargo_bin("echor").unwrap();
+  cmd.arg("Hello there").assert().success().stdout(expected);
 }
